45fe1ba2230ec3761f2a9a27395f9dc930d760f8
fionalin0221 <fionalin0221@gmail.com>
2025-04-03 23:55:01 +0800

    Add all files

---
+++ eval_time_limit.py
@@ -0,0 +1,109 @@
+from xml.etree import ElementTree as ET
+import importlib.util
+import sys
+import os
+import requests
+import argparse
+
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = JoypadSpace(env, COMPLEX_MOVEMENT)
+
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Mario")
+
+    parser.add_argument(
+        "--token",
+        default="",
+        type=str,
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+# args = parse_arguments()
+
+# retrive submission meta info
+# xml_file_path = 'meta.xml'
+
+# tree = ET.parse(xml_file_path)
+# root = tree.getroot()
+
+# sub_name = ""
+
+# for book in root.findall('info'):
+#     sub_name =  book.find('name').text
+
+# initializing agent
+# agent_path = sub_name + "_hw2_test.py"
+agent_path = "test_no_stack.py"
+module_name = agent_path.replace('/', '.').replace('.py', '')
+spec = importlib.util.spec_from_file_location(module_name, agent_path)
+module = importlib.util.module_from_spec(spec)
+sys.modules[module_name] = module
+spec.loader.exec_module(module)
+Agent = getattr(module, 'Agent')
+
+os.environ["SDL_AUDIODRIVER"] = "dummy"
+
+# evaluating
+import time
+from tqdm import tqdm
+
+total_reward = 0
+total_time = 0
+agent = Agent()
+time_limit = 120
+
+for episode in tqdm(range(2), desc="Evaluating"):
+    obs = env.reset()
+    start_time = time.time()
+    episode_reward = 0
+    step = 0
+    
+    while True:
+        action = agent.act(obs) 
+
+        obs, reward, done, info = env.step(action)
+        episode_reward += reward
+        step += 1
+        if step % 1000 == 0:
+            print(time.time() - start_time)
+
+        if time.time() - start_time > time_limit:
+            print(f"Time limit reached for episode {episode}")
+            break
+
+        if done:
+            break
+        
+        env.render()
+
+    end_time = time.time()
+    total_reward += episode_reward
+    total_time += (end_time - start_time)
+
+env.close()
+
+score = total_reward / 2
+print(f"Final Score: {score}")
+
+# push to leaderboard
+# params = {
+#     'act': 'add',
+#     'name': sub_name,
+#     'score': str(score),
+#     'token': args.token
+# }
+# url = 'http://project.aseart.com/s/FOB2023/deep/action.php'
+
+# response = requests.get(url, params=params)
+# if response.ok:
+#     print('Success:', response.text)
+# else:
+#     print('Error:', response.status_code)
---
+++ test_with_env.py
@@ -0,0 +1,55 @@
+import gym
+import torch
+from collections import deque
+from train import DQNSolver
+import numpy as np
+from torchvision import transforms
+import cv2
+
+# transform = transforms.Compose([
+#     transforms.ToPILImage(),                                   # Convert NumPy to PIL Image
+#     transforms.Grayscale(num_output_channels=1),
+#     transforms.Resize((90, 84)),
+#     transforms.CenterCrop(84),
+#     transforms.ToTensor(),                                     # Convert to tensor and normalize to [0, 1]
+# ])
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class Agent(object):
+    """Agent that acts randomly."""
+    def __init__(self):
+        self.action_space = gym.spaces.Discrete(12)
+        self.frames = deque([], maxlen=4)
+
+        self.model = torch.jit.load('policy_model_latest.pth').to(device)
+        self.model.eval()  # Set to evaluation mode
+        self.prev_obs = None
+        # pass
+
+    def act(self, observation):
+        # if self.prev_obs:
+        #     obs_np = np.array(observation)
+        #     prev_obs_np = np.array(self.prev_obs)
+        #     print(np.linalg.norm(prev_obs_np - obs_np))
+        # self.prev_obs = observation
+        # frame = cv2.cvtColor(observation, cv2.COLOR_RGB2GRAY)
+        # frame = cv2.resize(frame, (84, 90), interpolation=cv2.INTER_AREA)
+        # frame = frame[3:87, :]
+        # frame = np.expand_dims(frame, axis=0)
+
+        # self.frames.append(frame)
+        # while len(self.frames) < 4:
+        #     self.frames.append(frame)
+
+        # print(f"Initial frames stacked shape: {[frame.shape for frame in self.frames]}")  # Debugging
+
+        # input = np.concatenate(list(self.frames), axis=0)
+        input = observation
+        input = torch.tensor(input, dtype=torch.float32).unsqueeze(0) / 255.0
+        # print(input.shape)
+        
+        action_prob = self.model(input.to(device))
+        # print(action_prob)
+        return torch.argmax(action_prob).item()
+        # return self.action_space.sample()
